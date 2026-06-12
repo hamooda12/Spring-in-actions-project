aab86b599ba3eba2c9a6240586ff1c30d81e17a4
hamooda12 <tourt2031@gmail.com>
2026-06-12 15:17:36 +0300

    Add Spring Security to the project with basic configuration

---
+++ src/main/java/com/example/demo/SecurityController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("security")
+public class SecurityController {
+    @GetMapping("")
+    public String getHallow(){
+        return "Hello World";
+    }
+}

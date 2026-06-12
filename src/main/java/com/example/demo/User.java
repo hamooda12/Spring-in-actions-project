fe1115551fcd96cacff93ee9d8703cd8d26fa445
hamooda12 <tourt2031@gmail.com>
2026-06-12 15:41:15 +0300

    Add User security, and User entity

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String authority;
+
+
+
+}
